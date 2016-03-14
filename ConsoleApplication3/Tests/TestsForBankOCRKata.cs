643ae8eba3f2d2a29fd9e91ac8b8b9c41c8be8bd
Max P Haley <mphaley@us.ibm.com>
2016-03-14 09:41:54 -0400

    Refactoring and Notes on the Concept of Empty Shelled Methods

--- ConsoleApplication3/Tests/TestsForBankOCRKata.cs
+++ ConsoleApplication3/Tests/TestsForBankOCRKata.cs
@@ -18,113 +18,45 @@ namespace Tests
             var drawnNumberCharacters = new DrawnNumberCharacters();
 
             var bankocr = new BankOCR();
-            bankocr.ReadFile("firstOCRFile.txt");
-            bankocr.AssignCharactersToIndex();
-            var result = bankocr.ParseAllCharacters();
+            var contents = bankocr.ReadFile("firstOCRFile.txt");
+            var characters = bankocr.AssignCharactersToIndex(contents);
+            var result = bankocr.ParseAllCharacters(characters);
             Assert.AreEqual(111111111, result);
         }
+
         [Test]
         public void CanParseAllCharactersAtOnce()
         {
             var drawnNumberCharacters = new DrawnNumberCharacters();
 
             var bankocr = new BankOCR();
-            bankocr.ReadFile("secondOCRFile.txt");
-            bankocr.AssignCharactersToIndex();
-            var result = bankocr.ParseAllCharacters();
+            var contents = bankocr.ReadFile("secondOCRFile.txt");
+            var characters = bankocr.AssignCharactersToIndex(contents);
+            var result = bankocr.ParseAllCharacters(characters);
             Assert.AreEqual(123456789, result);
-        }
-
-        [Test]
-        public void TestingZerothCharacter()
-        {
-            var bankOCR = new BankOCR();
-            var drawnNumberCharacters = new DrawnNumberCharacters();
-            bankOCR.ReadFile("firstOCRFile.txt");
-            var TextLines = bankOCR.TextLines;
-            var zerothCharacter = bankOCR.GetNthCharacter(0);
-            List<string> expected = new List<string>
-            {
-                "   ",
-                "  |",
-                "  |",
-                "   "
-            };
-
-            Assert.AreEqual(expected, zerothCharacter);
         }
 
         [Test]
         public void TestingFirstOCRFileFirstLine()
         {
             var bankOCR = new BankOCR();
-            bankOCR.ReadFile("firstOCRFile.txt");
-            List<string> textStrings = bankOCR.TextLines;
-            string actualString =
-              "                           ";
+            var textStrings = bankOCR.ReadFile("firstOCRFile.txt");
+            string actualString = "                           ";
             Assert.AreEqual(textStrings[0], actualString);
         }
 
-        [Test]
-        public void TestingFirstOCRFileSecondLine()
-        {
-            var bankOCR = new BankOCR();
-            bankOCR.ReadFile("firstOCRFile.txt");
-            List<string> textStrings = bankOCR.TextLines;
-            string actualString =
-              "  |  |  |  |  |  |  |  |  |";
-            Assert.AreEqual(textStrings[2], actualString);
-        }
-
-        [Test]
-        public void TestingFirstOCRFinalListTest()
-        {
-            var bankOCR = new BankOCR();
-            bankOCR.ReadFile("firstOCRFile.txt");
-            List<string> textStrings = bankOCR.TextLines;
-            List<string> actualList = new List<string> {
-        "                           ",
-        "  |  |  |  |  |  |  |  |  |",
-        "  |  |  |  |  |  |  |  |  |",
-        "                           "
-      };
-            Assert.AreEqual(actualList, textStrings);
-        }
-
-        [Test]
-        public void GetNthCharacter_FirstCharacter()
-        {
-            var bankOCR = new BankOCR();
-            var input = new List<string> {
-              "111222333",
-              "444555666",
-              "777888999",
-              "         "   };
-
-            var expected = new List<string> {
-              "111",
-              "444",
-              "777",
-              "   "  };
-
-            bankOCR.TextLines = input;
-            Assert.AreEqual(expected, bankOCR.GetNthCharacter(0));
-            //bankOCR.GetNthCharacter(0));
-            //this test is not passing because the input is not returning anything in the ()
-            //the error says the index is out of range, which is because it's failing to print
-        }
-        [Test]
+         [Test]
         public void TestingConvertingOneLineAtATime()
         {
             var bankOCR = new BankOCR();
-            bankOCR.ReadFile("firstOCRFile.txt");
+            var contents = bankOCR.ReadFile("firstOCRFile.txt");
             List<string> actualList = new List<string> {
                   "                           ",
                   "  |  |  |  |  |  |  |  |  |",
                   "  |  |  |  |  |  |  |  |  |",
                   "                           "
                         };
-            Assert.AreEqual(actualList, bankOCR.TextLines);
+            Assert.AreEqual(actualList, contents);
         }
 
         [Test]
@@ -137,37 +69,7 @@ namespace Tests
         "  |",
         "  |",
         "   " };
-            // bankOCR.TextLines = testForDrawnNumberOne; 
             Assert.AreEqual(testForDrawnNumberOne, drawnNumberCharacters.Numbers[1]);
-        }
-
-
-        [Test]
-        public void TestingSubstrings()
-        {
-            string test = "123456789";
-
-            Assert.AreEqual("123", test.Substring(0, 3));
-        }
-
-        [Test]
-        public void GetNthCharacter_SecondCharacter()
-        {
-            var bankOCR = new BankOCR();
-            var input = new List<String> {
-                "111222333",
-                "444555666",
-                "777888999",
-                "         "
-                  };
-            var expected = new List<String> {
-                "222",
-                "555",
-                "888",
-                "   "
-                   };
-            bankOCR.TextLines = input;
-            Assert.AreEqual(expected, bankOCR.GetNthCharacter(1));
         }
     }
 }
