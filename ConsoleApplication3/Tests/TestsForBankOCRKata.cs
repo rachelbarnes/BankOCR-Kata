31f7ed0a990f15b1d5b50cc7a080fe4ed85d0e15
unknown <Administrator@phiws0035.sst.stp>
2016-03-16 08:10:13 -0400

    Starting it over again

--- ConsoleApplication3/Tests/TestsForBankOCRKata.cs
+++ ConsoleApplication3/Tests/TestsForBankOCRKata.cs
@@ -15,61 +15,9 @@ namespace Tests
         [Test]
         public void CanParseAllCharactersAtOnceJustOnes()
         {
-            var drawnNumberCharacters = new DrawnNumberCharacters();
-
             var bankocr = new BankOCR();
-            var contents = bankocr.ReadFile("firstOCRFile.txt");
-            var characters = bankocr.AssignCharactersToIndex(contents);
-            var result = bankocr.ParseAllCharacters(characters);
+            var result = bankocr.ParseFile("firstOCRFile.txt");
             Assert.AreEqual(111111111, result);
-        }
-
-        [Test]
-        public void CanParseAllCharactersAtOnce()
-        {
-            var drawnNumberCharacters = new DrawnNumberCharacters();
-
-            var bankocr = new BankOCR();
-            var contents = bankocr.ReadFile("secondOCRFile.txt");
-            var characters = bankocr.AssignCharactersToIndex(contents);
-            var result = bankocr.ParseAllCharacters(characters);
-            Assert.AreEqual(123456789, result);
-        }
-
-        [Test]
-        public void TestingFirstOCRFileFirstLine()
-        {
-            var bankOCR = new BankOCR();
-            var textStrings = bankOCR.ReadFile("firstOCRFile.txt");
-            string actualString = "                           ";
-            Assert.AreEqual(textStrings[0], actualString);
-        }
-
-         [Test]
-        public void TestingConvertingOneLineAtATime()
-        {
-            var bankOCR = new BankOCR();
-            var contents = bankOCR.ReadFile("firstOCRFile.txt");
-            List<string> actualList = new List<string> {
-                  "                           ",
-                  "  |  |  |  |  |  |  |  |  |",
-                  "  |  |  |  |  |  |  |  |  |",
-                  "                           "
-                        };
-            Assert.AreEqual(actualList, contents);
-        }
-
-        [Test]
-        public void TestingDictionaryMatrixCharacter()
-        {
-            var bankOCR = new BankOCR();
-            var drawnNumberCharacters = new DrawnNumberCharacters();
-            List<string> testForDrawnNumberOne = new List<string> {
-        "   ",
-        "  |",
-        "  |",
-        "   " };
-            Assert.AreEqual(testForDrawnNumberOne, drawnNumberCharacters.Numbers[1]);
         }
     }
 }
